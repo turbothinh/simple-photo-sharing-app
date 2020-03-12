aafad463d1099144b742550ff69fea7740dc6405
Turbo <turbothinh@gmail.com>
2020-03-12 14:18:14 +0100

    Add Emotion to app

--- App.tsx
+++ App.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import styled from '@emotion/native';
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>My First React Native Expo app!</Text>
-		</View>
+		<Container>
+			<Photo source={{ uri: 'http://placekitten.com/200/300' }} />
+			<Description>Featured photo!</Description>
+		</Container>
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
+const Photo = styled.Image`
+	width: 200px;
+	height: 200px;
+`;
+
+const Container = styled.View`
+	background-color: '#fff';
+	display: 'flex';
+	align-items: 'center';
+	justify-content: 'center';
+`;
+
+const Description = styled.Text`
+	font-size: 13px;
+`;
